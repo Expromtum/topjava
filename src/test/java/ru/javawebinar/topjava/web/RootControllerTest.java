581f6e71e8c5cf704a7280ab7ff237177292d4d8
Marina Sokurenko <expromtum@gmail.com>
2019-08-03 19:13:34 +0300

    HW07 RootControllerTest.testMeals for meals.jsp

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 
 import java.util.List;
@@ -9,8 +10,8 @@ import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.*;
-import static ru.javawebinar.topjava.MealTestData.MEAL1;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 class RootControllerTest extends AbstractControllerTest {
@@ -39,15 +40,14 @@ class RootControllerTest extends AbstractControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("meals"))
                 .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
-                .andExpect(model().attribute("meals", hasSize(6)))
-                .andExpect(model().attribute("meals", hasItem(
-                        allOf(
-                                hasProperty("id", is(MEAL1.getId())),
-                                hasProperty("description", is(MEAL1.getDescription())),
-                                hasProperty("dateTime", is(MEAL1.getDateTime())),
-                                hasProperty("calories", is(MEAL1.getCalories()))
-                        )
-                )))
+                .andExpect(model().attribute("meals",
+                        new AssertionMatcher<List<Meal>>() {
+                            @Override
+                            public void assertion(List<Meal> actual) throws AssertionError {
+                                assertMatch(actual, MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1);
+                            }
+                        }
+                ))
         ;
     }   
 }
