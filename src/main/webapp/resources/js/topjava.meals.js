966134e7c156e393c4448deea6433c0f5bc016aa
Marina Sokurenko <expromtum@gmail.com>
2019-12-16 14:54:27 +0300

    1: Перевести meals на datatables (meals.jsp, MealAjaxController). 1.1 Реализовать добавление записи еды через модальное окно Bootstrap и удаление еды по ajax (БЕЗ редактирования). 1.2 При вставке данных по AJAX пропадает все JSP форматирование, чинить перерисовку НЕ надо. Следующий урок- будем делать datatable по AJAX и форматирование на стороне клиента. 2: Т.к. HTML атрибут id у каждого элемента документа должен быть уникален, нужно избавиться от дублирования id="${user.id}" в строках таблиц users (users.jsp) (переместить атрибут id в тэг <tr> или передавать в качестве параметра функций через onclick)

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,30 @@
+$(function () {
+    makeEditable({
+            ajaxUrl: "ajax/meals/",
+            datatableApi: $("#datatable").DataTable({
+                "paging": false,
+                "info": true,
+                "order": [[ 0, "desc" ]],
+                "scrollY": 200,
+                "columns": [
+                    {
+                        "data": "dateTime"
+                    },
+                    {
+                        "data": "description"
+                    },
+                    {
+                        "data": "calories"
+                    }, {
+                        "defaultContent": "Edit",
+                        "orderable": false
+                    },
+                    {
+                        "defaultContent": "Delete",
+                        "orderable": false
+                    }
+                ],
+            })
+        }
+    );
+});
