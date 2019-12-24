9c53d40366f0276cb7eb95bfe7ebe6bec46ef3f1
Marina Sokurenko <expromtum@gmail.com>
2019-12-24 22:58:56 +0300

    HW09 2: Перевести meals.jsp на работу по ajax. Стиль строки таблицы сделать в зависимости от excess, время отображать без T.

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -29,9 +29,17 @@ public class RootController {
     }
 
     @GetMapping("/meals")
+    public String getMeals() {
+        return "meals";
+    }
+
+/*
+    @GetMapping("/meals")
     public String getMeals(Model model) {
         model.addAttribute("meals",
                 MealsUtil.getTos(mealService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
         return "meals";
     }
+    */
+
 }
